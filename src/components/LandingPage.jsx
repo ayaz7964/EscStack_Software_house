6630c62557661e2e731e540f0aca28bcf8346028
Ayaz Hussain <ayazhussain.bssef22@iba-suk.edu.pk>
2025-11-17 11:25:03 +0500

    Comment out BrandScroller component LandingPage
    Comment out the BrandScroller component in LandingPage.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,7 @@ function LandingPage() {
   return (
     <div>
       <HeroSection />
-      <BrandScroller />
+      {/* <BrandScroller /> */}
       <EscStackLanding />
       <ContactSidebar />
     </div>
